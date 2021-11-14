a58527c8b5ad3691b8c8ef1b98af984c66e860d3
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-11-14 22:10:06 +0200

    continue app

---
+++ src/components/AddTask.vue
@@ -0,0 +1,67 @@
+<template>
+    <form action="" class="form add-form">
+        <div class="form-control">
+            <label for="">Task</label>
+            <input type="text" name="text" placeholder="Add Task">
+        </div>
+        <div class="form-control">
+            <label for="">Day & Time</label>
+            <input type="text" name="day" placeholder="Add Day & Time">
+        </div>
+        <div class="form-control form-control-check">
+            <label for="">Set Reminder</label>
+            <input type="checkbox" name="reminder">
+        </div>
+        <input type="submit" value="Save Task" class="btn btn--block">
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'AddTask',
+    data() {
+        return {
+            text: '',
+            day: '',
+            reminder: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.add-form {
+    margin-bottom: 30px;
+}
+
+.form-control {
+    margin-bottom: 15px;
+}
+
+.form-control label {
+    display: block;
+    line-height: 1.5;
+    font-size: 16px;
+    font-weight: 600;
+}
+
+.form-control input {
+    width: 100%;
+    height: 40px;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 17px;
+}
+.form-control-check {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.form-control-check label {
+    flex: 1;
+}
+.form-control-check input {
+    flex: 2;
+    height: 20px;
+}
+</style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="task">
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
         <div class="task__head">
             <h3>{{task.text}}</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 50 50" width="20px" height="20px"><path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"/></svg>
+            <svg @click="$emit('delete-task', task.id)" class="task__delete" xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 50 50" width="20px" height="20px"><path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"/></svg>
         </div>
         <p>{{task.day}}</p>
     </div>
@@ -14,7 +14,7 @@ export default {
     name: 'Task',
     props: {
         task: Object
-    }
+    },
 }
 </script>
 
@@ -23,11 +23,21 @@ export default {
      margin: 5px;
      background-color: #f1f1f1;
      padding: 10px 15px;
+     border-left: 5px solid transparent;
+ }
+
+ .reminder {
+     border-left: 5px solid green;
  }
 
  .task__head {
      display: flex;
      align-items: center;
      justify-content: space-between;
+ }
+
+ .task__delete {
+     cursor: pointer;
+     fill: red;
  }
 </style>
