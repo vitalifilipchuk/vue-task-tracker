299bfa6f1f55cd7728016125379ae5351cc9d62e
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-11-14 23:01:59 +0200

    finished basic UI

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,16 +1,16 @@
 <template>
-    <form action="" class="form add-form">
+    <form @submit="onSubmit" class="form add-form">
         <div class="form-control">
             <label for="">Task</label>
-            <input type="text" name="text" placeholder="Add Task">
+            <input type="text" v-model="text" name="text" placeholder="Add Task">
         </div>
         <div class="form-control">
             <label for="">Day & Time</label>
-            <input type="text" name="day" placeholder="Add Day & Time">
+            <input type="text" v-model="day" name="day" placeholder="Add Day & Time">
         </div>
         <div class="form-control form-control-check">
             <label for="">Set Reminder</label>
-            <input type="checkbox" name="reminder">
+            <input type="checkbox" v-model="reminder" name="reminder">
         </div>
         <input type="submit" value="Save Task" class="btn btn--block">
     </form>
@@ -24,6 +24,29 @@ export default {
             text: '',
             day: '',
             reminder: false
+        }
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault();
+
+            if (!this.text) {
+                alert('Add a task first!')
+                return
+            }
+
+            const newTask = {
+                id: Math.floor(Math.random() * 100000),
+                text: this.text,
+                day: this.day,
+                reminder: this.reminder
+            }
+
+            this.text = ''
+            this.day = ''
+            this.reminder = false
+
+            this.$emit('add-task', newTask)
         }
     }
 }
@@ -48,7 +71,7 @@ export default {
 .form-control input {
     width: 100%;
     height: 40px;
-    margin: 5px;
+    margin: 5px 0;
     padding: 3px 7px;
     font-size: 17px;
 }
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,7 +11,7 @@
         },
         methods: {
             onClick() {
-                
+                this.$emit('btn-click')
             }
         }
     }
